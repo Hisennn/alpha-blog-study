3eaee2ce39fbca19324d0ab3178232747381f8b3
Hisennn <gabrielsilvarz@outlook.com>
2025-05-26 14:58:38 -0300

    delete and show links

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,11 +6,42 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
   def new
+    @article = Article.new
+  end
 
+  def edit
+    @article = Article.find(params[:id])
   end
+
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
-    @article.save
-    redirect_to @article
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = "Article was successfully created."
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated."
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+
 end
