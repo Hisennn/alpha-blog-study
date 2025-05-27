7c4a65a5957e40b7281980b4ae7d684d513c2c4b
Hisennn <gabrielsilvarz@outlook.com>
2025-05-27 15:00:44 -0300

    before_action  code in articles_controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
   def show
-    @article = Article.find(params[:id])
   end
   def index
     @articles = Article.all
@@ -10,7 +10,6 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def create
@@ -24,7 +23,6 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:notice] = "Article was successfully updated."
       redirect_to @article
@@ -34,14 +32,18 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
+    flash[:notice] = "Article was successfully destroyed."
     redirect_to articles_path
   end
 
   private
   def article_params
     params.require(:article).permit(:title, :description)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 
 end
