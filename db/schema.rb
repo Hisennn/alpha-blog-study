551a3d79138719ab2b8e85edddfc7e5f182d8e0a
Hisennn <gabrielsilvarz@outlook.com>
2025-06-12 15:29:55 -0300

    added passsword column and secure_password

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_02_203531) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_12_182127) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -28,6 +28,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_02_203531) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "articles", "users"
